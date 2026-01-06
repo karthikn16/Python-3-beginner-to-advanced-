6b5577904d38924657d7ad6f1adb5b34f6aaa117
karthikeyan <81953615+karthikn16@users.noreply.github.com>
2026-01-06 19:54:43 +0530

    Update type_casting.py

--- type_casting.py
+++ type_casting.py
@@ -1,5 +1,5 @@
 name,day,year ="nan",15,2021
-#print{"Dear" +name+", \nou have"+" "+day +"days of leave balance for this \nyear"+" (" +year+ ")."}
+print{"Dear" +name+", \nou have"+" "+day +"days of leave balance for this \nyear"+" (" +year+ ")."}
 name = "anand"
 day = "15"
 year = "2021"
@@ -7,4 +7,5 @@ year = "2021"
 print("Dear " + name + ",\nYou have " + day + " days of leave balance for this\nyear (" + year + ").")
 
 name = input("whats your name")
+
 print("your name :" + name)
